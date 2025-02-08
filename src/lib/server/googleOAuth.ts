30dfe513eebfafe6b4e3ed8e34fcda5d5674a534
Brady Holt <brady.holt@gmail.com>
2025-02-08 15:08:55 -0600

    Extract Google OAuth callback code into a module

---
+++ src/lib/server/googleOAuth.ts
@@ -0,0 +1,48 @@
+import type { OAuth2Tokens } from "arctic";
+import { Google, decodeIdToken } from "arctic";
+
+export type IGoogleOAuthCallbackParams = {
+  code: string | null;
+  state: string | null;
+  storedState: string | null;
+  codeVerifier: string | null;
+};
+
+export type IGoogleOAuthConfig = {
+  clientId: string;
+  clientSecret: string;
+  redirectUri: string;
+};
+
+export async function handleOAuthCallBack(
+  posted: IGoogleOAuthCallbackParams,
+  googleConfig: IGoogleOAuthConfig
+): Promise<{ googleId: string; name: string; email: string } | null> {
+  if (
+    posted?.code === null ||
+    posted?.state === null ||
+    posted?.storedState === null ||
+    posted?.codeVerifier === null
+  ) {
+    return null;
+  }
+  if (posted?.state !== posted?.storedState) {
+    return null;
+  }
+
+  let tokens: OAuth2Tokens;
+  try {
+    const google = new Google(googleConfig.clientId, googleConfig.clientSecret, googleConfig.redirectUri);
+    tokens = await google.validateAuthorizationCode(posted.code, posted.codeVerifier);
+  } catch (e) {
+    // Invalid code or client credentials
+    return null;
+  }
+  const claims: any = decodeIdToken(tokens.idToken());
+
+  return {
+    googleId: claims.sub,
+    name: claims.name,
+    email: claims.email,
+  };
+}
--- src/routes/login/google/callback/+server.ts
+++ src/routes/login/google/callback/+server.ts
@@ -1,69 +1,57 @@
 import { generateSessionToken, createSession, setSessionTokenCookie } from "$lib/server/session";
 import { drizzle } from "drizzle-orm/d1";
-import { decodeIdToken } from "arctic";
 import { users } from "$lib/server/schema/users";
 import { eq } from "drizzle-orm";
+import { handleOAuthCallBack } from  "$lib/server/googleOAuth";
 import type { RequestEvent } from "@sveltejs/kit";
-import type { OAuth2Tokens } from "arctic";
-import { Google } from "arctic";
 
 export async function GET(event: RequestEvent): Promise<Response> {
   const code = event.url.searchParams.get("code");
   const state = event.url.searchParams.get("state");
   const storedState = event.cookies.get("google_oauth_state") ?? null;
   const codeVerifier = event.cookies.get("google_code_verifier") ?? null;
-  if (code === null || state === null || storedState === null || codeVerifier === null) {
-    return new Response(null, {
-      status: 400,
-    });
-  }
-  if (state !== storedState) {
-    return new Response(null, {
-      status: 400,
-    });
-  }
+  const googleAuth = await handleOAuthCallBack(
+    {
+      code,
+      state,
+      storedState,
+      codeVerifier,
+    },
+    {
+      clientId: event.platform?.env.GOOGLE_CLIENT_ID!,
+      clientSecret: event.platform?.env.GOOGLE_CLIENT_SECRET!,
+      redirectUri: event.platform?.env.GOOGLE_REDIRECT_URI!,
+    }
+  );
 
-  let tokens: OAuth2Tokens;
-  try {
-    const google = new Google(
-      event.platform?.env.GOOGLE_CLIENT_ID!,
-      event.platform?.env.GOOGLE_CLIENT_SECRET!,
-      event.platform?.env.GOOGLE_REDIRECT_URI!
-    );
-    tokens = await google.validateAuthorizationCode(code, codeVerifier);
-  } catch (e) {
-    // Invalid code or client credentials
+  if (!googleAuth) {
     return new Response(null, {
       status: 400,
     });
   }
-  const claims: any = decodeIdToken(tokens.idToken());
-  const googleId = claims.sub;
-  const name = claims.name;
-  const email = claims.email;
 
   const db = drizzle(event.platform?.env.DB);
-  const [existingUser] = await db.select().from(users).where(eq(users.googleId, googleId));
+  const [existingUser] = await db.select().from(users).where(eq(users.googleId, googleAuth.googleId));
 
   if (existingUser !== undefined) {
-    await db.update(users).set({ name, email }).where(eq(users.id, existingUser.id));
+    // Existing user
+    await db.update(users).set({ name: googleAuth.name, email: googleAuth.email }).where(eq(users.id, existingUser.id));
 
     const sessionToken = generateSessionToken();
-    const session = await createSession(db, sessionToken, existingUser.id);
+    const session = await createSession(sessionToken, existingUser.id);
     setSessionTokenCookie(event, sessionToken, session.expiresAt);
-    return new Response(null, {
-      status: 302,
-      headers: {
-        Location: "/",
-      },
-    });
+  } else {
+    // New user
+    const [{ insertedId }] = await db
+      .insert(users)
+      .values({ name: googleAuth.name, email: googleAuth.email, googleId: googleAuth.googleId })
+      .returning({ insertedId: users.id });
+
+    const sessionToken = generateSessionToken();
+    const session = await createSession(sessionToken, insertedId);
+    setSessionTokenCookie(event, sessionToken, session.expiresAt);
   }
 
-  const [{ insertedId }] = await db.insert(users).values({ name, email, googleId }).returning({ insertedId: users.id });
-
-  const sessionToken = generateSessionToken();
-  const session = await createSession(db, sessionToken, insertedId);
-  setSessionTokenCookie(event, sessionToken, session.expiresAt);
   return new Response(null, {
     status: 302,
     headers: {
