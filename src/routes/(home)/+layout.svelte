23e375b4ed16bad7512266a02d83a3c15cb10edb
Brady Holt <brady.holt@gmail.com>
2025-02-12 20:09:46 -0600

    More initial build-out

---
+++ src/routes/(home)/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">  
+  let { children } = $props();
+</script>
+
+<header class="container">
+  <nav>
+    <ul>
+      <li><h1>Snoozer</h1></li>
+    </ul>
+    <ul>
+      <li>
+				<a href="/logout">Logout</a>        
+      </li>
+    </ul>
+  </nav>
+</header>
+{@render children()}
+
+<style>
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,18 +8,6 @@
   <meta name="description" content="Snoozer" />
 </svelte:head>
 
-<header class="container">
-  <nav>
-    <ul>
-      <li><h1>Snoozer</h1></li>
-    </ul>
-    <ul>
-      <li>
-				<a href="/logout">Logout</a>        
-      </li>
-    </ul>
-  </nav>
-</header>
 {@render children()}
 
 <style>
